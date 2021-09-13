51394a7d497fca2df51e06e10f6dedc0780cab61
Ramona Hirvonen <61796422+Ramseskus@users.noreply.github.com>
2021-09-13 15:02:45 +0300

    Update App.js

--- addressFinder/App.js
+++ addressFinder/App.js
@@ -18,7 +18,7 @@ export default function App() {
 
   const getLocation = () => {
     const url =
-      "http://www.mapquestapi.com/geocoding/v1/address?key=oXxeShecQm439ah7SXIpjgJkqmPE4Tf1" +
+      "http://www.mapquestapi.com/geocoding/v1/address?key=*****" +
       "&location=" +
       title;
     fetch(url)
